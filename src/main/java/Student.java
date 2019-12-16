c9e4b162ba09ac8ffd3d907f635f3f0c160491cf
andrewswint <andrewswint1976@gmail.com>
2019-12-16 11:52:07 -0600

    tests for basic information

--- src/main/java/Student.java
+++ src/main/java/Student.java
@@ -1,2 +1,25 @@
 public class Student {
+
+    private long userID;
+    private String userName;
+    private double userGrade;
+
+    public Student(long userID, String userName, double userGrade) {
+        this.userID = userID;
+        this.userName = userName;
+        this.userGrade = userGrade;
+    }
+
+    public long getUserID() {
+        return userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public double getUserGrade() {
+        return userGrade;
+    }
+
 }
--- test/java/StudentTest.java
+++ test/java/StudentTest.java
@@ -4,14 +4,27 @@ import static org.junit.Assert.*;
 
 public class StudentTest {
 
+    Student student1;
+    Student student2;
+
+    @Before
+    public void initStudentConstruction () {
+        student1 = new Student(1111L, "student1", 90.5);
+        student2 = new Student(1112L, "student2", 80.5);
+    }
+
     @Test
     public void testUserID () {
+        assertEquals(1111L, student1.getUserID());
+        assertNotEquals(student1, student2);
 
+//        long random = student1.getUserID();
+//        assertSame(student1.getUserID(), random);
     }
 
     @Test
     public void testUserName () {
-
+        assertEquals("student1", student1.getUserName());
     }
 
     @Test

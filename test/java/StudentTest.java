a9fc77c77e9fb0e9740f483000077a0a1953d18a
andrewswint <andrewswint1976@gmail.com>
2019-12-16 11:23:50 -0600

    basic setup for exersice

---
+++ test/java/StudentTest.java
@@ -0,0 +1,22 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class StudentTest {
+
+    @Test
+    public void testUserID () {
+
+    }
+
+    @Test
+    public void testUserName () {
+
+    }
+
+    @Test
+    public void testUserGrades () {
+
+    }
+
+}
